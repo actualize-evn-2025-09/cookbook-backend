a12f507e4bcc991e1764a7a253ec69263315dc18
leonshimizu <lmshimizu@gmail.com>
2025-12-03 12:31:12 +1000

    Implement login and logout (sessions controller)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  def create # login
+    # find the user by their email
+    user = User.find_by(email: params[:email])
+
+    # if the user exists and they have a valid password - based on bcrypt
+    if user && user.authenticate(params[:password])
+      # start creating the cookie object
+      cookie  = { value: user.id }
+      # make sure that no one can tamper with the cookie and merge the cookie settings into the cookie object we created
+      cookies.signed[:user_id] = cookie.merge(cookie_settings)
+      render json: { email: user.email, user_id: user.id }, status: :created
+    else
+      render json: {}, status: :unauthorized # 401
+    end
+  end
+
+  def destroy
+    cookies.delete(:user_id, cookie_settings)
+    render json: { message: "Logged out successfully" }
+  end
+
+  private
+
+  def cookie_settings
+    # checks if we are in test mode (if we're running rails test)
+    if Rails.env.test?
+      { httponly: true }
+    else
+      { httponly: true, secure: true, same_site: "None" }
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
   has_secure_password # bcrypt - hashes password and allows us to use password and password_confirmation and adding validations for password and password_confirmation
   validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: /\A.+@actualize\.com\z/, message: "must be an @actualize.com email" }
 end
