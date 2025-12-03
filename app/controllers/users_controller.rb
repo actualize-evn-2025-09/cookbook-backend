7ef53e018577bec6642ba54f90fe7d0ec7e9413e
leonshimizu <lmshimizu@gmail.com>
2025-12-03 11:20:01 +1000

    Add users controller and tests - finish the 'signup'

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    if user.save
+      render json: { message: "User created successfully" }, status: :created # 201 created
+    else
+      render json: { errors: user.errors }, status: :bad_request # 400
+    end
+  end
+end
