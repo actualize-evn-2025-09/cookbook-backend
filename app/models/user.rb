2700e819fb57b56d2554715de1b21596abafdcbe
leonshimizu <lmshimizu@gmail.com>
2025-12-03 10:26:28 +1000

    Add the user model and start setting it up for authentication

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  has_secure_password # bcrypt - hashes password and allows us to use password and password_confirmation
+  validates :email, presence: true, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_21_011003) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_03_001541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_21_011003) do
     t.datetime "created_at", null: false
     t.string "image_url"
     t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
   end
 end
