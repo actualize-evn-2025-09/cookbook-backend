e0df398cc3d0693f0141d229ddca19efe0b2d41e
leonshimizu <lmshimizu@gmail.com>
2025-12-17 11:45:25 +1000

    Added seeddump and updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173"
+    origins "localhost:5173", "https://cookbook-frontend-actualize.netlify.app"
     resource "*",
       headers: :any,
       credentials: true,
